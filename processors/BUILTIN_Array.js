bc886ac8caf5ee7a4ec2bdec95a2dc50aab3a644
Dorian Cransac <reachme@doriancransac.info>
2019-09-23 12:48:24 +0200

    updating test

--- processors/BUILTIN_Array.js
+++ processors/BUILTIN_Array.js
@@ -7,26 +7,29 @@ var getUniqueId = function () {
 var oidkey = 'iiaoid';
 
 function IdIndexArray(arrayArg) {
-    if(!arrayArg){
-        arrayArg = [];
+    if (!arrayArg) {
+        throw 'Please provide array ref as argument.';
     }
     return {
         array: arrayArg,
         getId: function (obj) {
-            return obj[oidkey];
+            return obj['oid'];
         },
         addNew: function (obj) {
             var newId = getUniqueId();
-            obj[oidkey] = newId;
+            obj['oid'] = newId;
             this.array.push(obj);
             return newId;
+        },
+        addExisting: function (obj) {
+            this.array.push(obj);
         },
         clear: function () {
             this.array.length = 0;
         },
         getById: function (oid) {
             var index = this.getIndexById(oid);
-            if(index === -1){
+            if (index === -1) {
                 throw 'No object found with Id:' + oid;
             }
             return this.array[index];
@@ -36,35 +39,90 @@ function IdIndexArray(arrayArg) {
         },
         getIndexById: function (oid) {
             for (i = 0; i < this.array.length; i++) {
-                if (this.array[i][oidkey] === oid) {
+                if (this.array[i]['oid'] === oid) {
                     return i;
                 }
             }
             return -1;
         },
         getIdByIndex: function (idx) {
-            return this.array[idx][oidkey];
+            return this.array[idx]['oid'];
         },
         removeById: function (oid) {
             this.array.splice(this.getIndexById(oid), 1);
         },
-        copyById(oid) {
-            return jsoncopy(this.getById(oid));
+        removeByIndex: function (idx) {
+            var oid = this.getIdByIndex(idx);
+            this.array.splice(idx, 1);
         },
-        count() {
+        copyById: function (oid) {
+            var copy = jsoncopy(this.getById(oid));
+            copy['oid'] = getUniqueId();
+            return copy;
+        },
+        duplicateById: function (oid) {
+            var newId = this.addExisting(this.copyById(oid)); // pushed at the end of the array
+            var copyIdx = this.getIndexById(newId);
+            var originalIdx = this.getIndexById(oid);
+            this.moveFromToIndex(copyIdx, originalIdx + 1);
+        },
+        moveFromToIndex: function (old_index, new_index) {
+            this.array.splice(new_index, 0, this.array.splice(old_index, 1)[0]);
+        },
+        count: function () {
             return this.array.length;
         },
-        getPrevious(oid){
+        getPrevious: function (oid) {
             return this.getByIndex(this.getIndexById(oid) - 1);
         },
-        getPreviousId(oid){
-            return this.getPrevious(oid).getId();
+        getPreviousId: function (oid) {
+            return this.getId(this.getPrevious(oid));
         },
-        getNext(oid){
+        getNext: function (oid) {
             return this.getByIndex(this.getIndexById(oid) + 1);
         },
-        getNextId(oid){
-            return this.getNext(oid).getId();
+        getNextId: function (oid) {
+            return this.getId(this.getNext(oid));
+        },
+        addIfAbsent: function (obj) {
+            if (!(obj['oid'])) {
+                return this.addNew(obj);
+            }
+            var index = this.getIndexById(obj['oid']);
+            if (index < 0) {
+                return this.addExisting(obj);
+            }
+            //already present
+            return index;
+        },
+        removeIfExists: function (oid) {
+            if (!oid) {
+                throw 'Cant remove object without proper oid';
+            }
+            var index = this.getIndexById(oid);
+            if (index < 0) {
+                //doesn't exist
+                return -1;
+            }
+            //found
+            return this.removeByIndex(index);
+        },
+        removeAll: function () {
+            console.log('[idindexarray] removeAll called. collection size:' + this.array.length);
+            for (var i = 0; i < this.array.length; i++) {
+                try{
+                    console.log('[ITERATION '+i+']')
+                    this.removeByIndex(i);
+                }catch(e){
+                    console.log('error: ' + e);
+                }finally{
+                    console.log(i);
+                }
+            }
+            console.log('[idindexarray] removall finished.')
+        },
+        getAsArray: function(){
+            return this.array;
         }
     };
 };
