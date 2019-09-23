d32fd11973009ef3226ffac8c009e25421567623
Dorian Cransac <reachme@doriancransac.info>
2019-09-23 13:17:53 +0200

    adding missing file to dist

--- dist/viz.js
+++ dist/viz.js
@@ -1083,6 +1083,39 @@ angular.module('key-val-collection', ['ui.bootstrap'])
             }
         }
     });
+registerScript();
+
+angular.module('dashletcomssrv', [])
+    .service('dashletcomssrv', function ($rootScope) {
+ 
+        var dashletcomssrv = {};
+        dashletcomssrv.buffer = {};
+        dashletcomssrv.masters = {};
+        dashletcomssrv.masters.array = [];
+        dashletcomssrv.masters = new IdIndexArray(dashletcomssrv.masters.array, function(oid){
+            //console.log('[dashletcomssrv] removing ' + oid);
+        });
+
+        dashletcomssrv.registerWidget = function (dashletid, dashlettitle) {
+            console.log('[dashletcomssrv] registering ' + dashletid);
+            dashletcomssrv.masters.addIfAbsent({'oid' : dashletid, 'title': dashlettitle});
+        };
+
+        dashletcomssrv.udpdateTitle = function (dashletid, dashlettitle) {
+            dashletcomssrv.masters.getById(dashletid).title = dashlettitle;             
+        };
+
+        dashletcomssrv.unregisterWidget = function (dashletid) {
+            console.log('[dashletcomssrv] unregistering ' + dashletid);
+            dashletcomssrv.masters.removeIfExists(dashletid);
+        };
+        
+        dashletcomssrv.updateMasterValue = function (dashletid, newValue) {
+            dashletcomssrv.buffer[dashletid] = newValue;
+        };
+
+        return dashletcomssrv;
+    });
 function IdIndexArray(arrayArg) {
     if (!arrayArg) {
         throw 'Please provide array ref as argument.';
