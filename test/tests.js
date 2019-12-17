76e0766ea3fca9d4d1c7a1d07a4ac00fd09da844
Dorian Cransac <reachme@doriancransac.info>
2019-12-17 14:02:45 +0100

    implementing first mocha-based test

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,21 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+var fs = require('fs');
+var vm = require('vm');
+const path = require("path");
+const file1 = fs.readFileSync(path.resolve(__dirname, '../src/js/core-classes.js'));
+const file2 = fs.readFileSync(path.resolve(__dirname, '../src/js/object-defaults.js'));
+
+vm.runInThisContext(file1 + '\n' + file2);
+
+describe('SpaceTime', function()
+{
+    describe('brabra', function()
+    {
+        it('something',
+            function()
+            {
+                assert.equal(3000, new DefaultGlobalSettings().intervalduration);
+            })
     });
-  });
+
 });
